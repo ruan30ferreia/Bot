398db106fb671fe050e91e7f66ce71904cfdd34a
Ruan Ferreira <ruan.hsferreira+conta2@gmail.com>
2020-04-08 13:33:37 -0300

    Diminui o código

--- bot_pack/messageHandling.py
+++ bot_pack/messageHandling.py
@@ -1,10 +1,5 @@
-abecedario = {'á': 'a', 'à': 'a', 'â': 'a', 'ã': 'a', 'Á': 'a', 'À': 'a', 'Â': 'a', 'Ã': 'a', 'é': 'e', 'è': 'e',
-              'ê': 'e', 'È': 'e', 'É': 'e', 'Ê': 'e', 'í': 'i', 'ì': 'i', 'î': 'i', 'Ì': 'i', 'Í': 'i', 'Î': 'i',
-              'ó': 'o', 'ò': 'o', 'ô': 'o', 'õ': 'o', 'Ò': 'o', 'Ó': 'o', 'Ô': 'o', 'Õ': 'o', 'ú': 'u',
-              'ù': 'u', 'û': 'u', 'Ù': 'u', 'Ú': 'u', 'Û': 'u', 'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e',
-              'F': 'f', 'G': 'g', 'H': 'h', 'I': 'i', 'J': 'j', 'K': 'k', 'L': 'l', 'M': 'm', 'N': 'n', 'O': 'o',
-              'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't', 'U': 'u', 'V': 'v', 'W': 'w', 'X': 'x', 'Y': 'y',
-              'Z': 'z'}
+abecedario = {'á': 'a', 'à': 'a', 'â': 'a', 'ã': 'a', 'é': 'e', 'è': 'e', 'ê': 'e', 'í': 'i', 'ì': 'i', 'î': 'i',
+              'ó': 'o', 'ò': 'o', 'ô': 'o', 'õ': 'o', 'ú': 'u', 'ù': 'u', 'û': 'u'}
 lista_acentos = ['`', '´', '~', '^', '?', '!']
 palavras_abreviadas = {'vc': 'voce', 'vm': 'vamo', 'blz': 'beleza', 'vms': 'vamos', 'hj': 'hoje', 'por que': 'porque'}
 t = ['vc', 'vm', 'blz', 'vms', 'hj']
@@ -12,6 +7,7 @@ t = ['vc', 'vm', 'blz', 'vms', 'hj']
 
 # Essa função troca algumas abreviações que tiverem na frase se tiverem e tira letras maiusuclas e acentos.
 def ABC(frase):
+    frase = frase.lower()
     letras_frase = list(frase)
     for i in abecedario:
         while i in letras_frase:
